a135adc45e19fb5efde08fb97358c0a9e4868bbf
ap20057 <ap20057@ues.edu.sv>
2026-03-05 15:53:33 -0600

    Worked on exercise two, Brother Marcos Alas

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,73 @@
 using System;
-
+// Start asking the grade to the user and show the letter of this one.
+/* 
+ Project: Grades Calculator in C#. 
+ Author: Brother Marcos Alas. 
+ Purpose: Use of variables, if statements and other elements learned for c#.
+ */
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //Here is for the grade provided by the user.
+        Console.Write("Please type your grade: ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+        //Variable for the letter of the grade.
+        string letter = "";
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        //adding sign + or - to the letter grade.
+        string sign = "";
+        int lastDigit = gradeNumber % 10;
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit <= 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        if (gradeNumber >= 93)
+        {
+            sign = "";
+        }
+        if (letter == "F")
+        {
+            sign = "";
+        }
+        Console.WriteLine($"Your grade is: {letter}{sign}. ");
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congratulations, Welcome to the champions team! ");
+        }
+        else
+        {
+            Console.WriteLine("Work harder to get it next time! ");
+        }
     }
 }
